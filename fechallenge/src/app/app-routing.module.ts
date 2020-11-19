82dd86c045bdfb458f09c51c2a69f309b1728478
sayan-datta-6672 <sayan.datta.engr@gmail.com>
2020-11-19 20:52:52 +0530

    set up structure

--- fechallenge/src/app/app-routing.module.ts
+++ fechallenge/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './helpers/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './account/layout.component';
+import { LoginComponent } from './account/login.component';
+import { RegisterComponent } from './account/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { 
+        path: 'home', 
+        component: HomeComponent, 
+        canActivate: [AuthGuard] 
+    },
+    {
+        path: 'account', component: LayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent }
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fechallenge/src/app/app.module.ts
+++ fechallenge/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthGuard } from './helpers/auth.guard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './account/layout.component';
+import { LoginComponent } from './account/login.component';
+import { RegisterComponent } from './account/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
